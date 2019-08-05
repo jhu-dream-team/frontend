1dd5d5c524e9cb31378fef985848abff5f6d9df4
Nicholas Frush <nick@frushco.de>
2019-08-05 01:12:46 +0000

    Created the login page for the app and tested it could receive input

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,9 @@ import * as ReactDOM from 'react-dom';
 require("./styles/index.scss");
 
 import App from './components/app';
+import Login from './components/login';
 
 ReactDOM.render (
-  <App />,
+  <Login />,
   document.getElementById("root")
 );
