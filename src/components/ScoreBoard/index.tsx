da3d227a880e8ae1d008473bc24beefc451d51dc
Ubuntu <ubuntu@ip-172-31-15-143.us-east-2.compute.internal>
2019-08-07 07:21:21 +0000

    Cleaned up score board component. Get request needs to be corrected

---
+++ src/components/ScoreBoard/index.tsx
@@ -0,0 +1,52 @@
+import * as React from 'react';
+
+class ScoreBoard extends React.Component<any, any> {
+
+    constructor(props){
+        super(props);
+        this.state = {
+	  error: null,
+	  isLoaded: false,
+	  items:[]
+        };
+     }	
+
+    componentDidMount(){
+	fetch("http://localhost:5000/games")
+	  .then(res => res.json())
+	  .then((result) => {
+	      this.setState({
+		isLoaded: true,
+		items: result
+	       });
+	     },
+	     (error) => {
+	       this.setState({
+	         isLoaded: true,
+		 error
+	       });
+	     }
+     }
+	
+
+    render() {
+	const { error, isLoaded, items } = this.state;
+	if (error){
+	   return <div>Error: {error.message}</div>;
+	}else if (!isLoaded){
+	   return <div> Loading...</div>;
+	}else{
+          return (
+	   <ul>
+	     {items.map(item => (
+		<li key={item.id}>
+		  {item.id}{item.score}
+		</li>
+	     ))}
+	   </ul>
+	 );
+	}
+       }
+     }
+
+export default ScoreBoard;
